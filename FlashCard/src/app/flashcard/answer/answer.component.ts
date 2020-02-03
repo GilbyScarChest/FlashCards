7c9e5537f70739ffc04c85388ddd36cdf7277060
Sergio De Anda <35745672+Sergio-De-Anda@users.noreply.github.com>
2020-02-03 15:16:33 -0600

    update component to display answer

--- FlashCard/src/app/flashcard/answer/answer.component.ts
+++ FlashCard/src/app/flashcard/answer/answer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Question } from 'src/Interfaces/Question';
 
 @Component({
   selector: 'app-answer',
@@ -6,12 +7,13 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./answer.component.css']
 })
 export class AnswerComponent implements OnInit {
-
+    @Input() currentQuestion: Question;
     @Output() modeOutput: EventEmitter<string> = new EventEmitter<string>();
-    
+
     constructor() { }
 
     ngOnInit() {
+      console.log(this.currentQuestion);
     }
 
     BackToSameCard(): void {
